180cf0390d5f8be010a68225afced8c4f9584b91
nicodwi_k <nicodwika@gmail.com>
2020-10-26 23:44:46 +0700

    update change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './assets/css/style.css';
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-6x1">Javascript React</h1>
+      <h1 className="text-6xl">Javascript React</h1>
     </div>
   );
 }
